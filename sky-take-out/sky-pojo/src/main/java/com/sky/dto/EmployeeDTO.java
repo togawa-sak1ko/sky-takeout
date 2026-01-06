ea1d1f121b21921b94638505749e02fdc4da9153
Simon <jabberwocky2@163.com>
2026-01-05 20:04:08 -0500

    新增员工相关业务代码

--- sky-take-out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-take-out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -7,16 +8,22 @@ import java.io.Serializable;
 @Data
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty("员工id")
     private Long id;
 
+    @ApiModelProperty("用户名")
     private String username;
 
+    @ApiModelProperty("员工名字")
     private String name;
 
+    @ApiModelProperty("手机号")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
 }
